53a70486bec7477b1d9a20711299e7d0bb052412
Dennis Hackethal <dh@dennishackethal.com>
2023-12-13 20:22:56 -0800

    Decrease animal spawn range

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -25,7 +25,7 @@ public class SpawnManager : MonoBehaviour
     {
         int index = Random.Range(0, prefabs.Length);
         GameObject prefab = prefabs[index];
-        Vector3 spawnPosition = new Vector3(Random.Range(-20, 20), 0, 20);
+        Vector3 spawnPosition = new Vector3(Random.Range(-7, 7), 0, 20);
 
         Instantiate(prefab, spawnPosition, prefab.transform.rotation);
     }

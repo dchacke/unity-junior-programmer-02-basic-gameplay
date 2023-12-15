7df787eedcfb1aa108f280fb5c311374b592c19a
Dennis Hackethal <dh@dennishackethal.com>
2023-12-14 20:38:20 -0800

    Prevent player from spamming spacebar

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,30 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private bool canSpawnDog = true;
+
+    void Start()
+    {
+        StartCoroutine("ResetSpawnability");
+    }
+
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawnDog)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canSpawnDog = false;
+        }
+    }
+
+    private IEnumerator ResetSpawnability()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            canSpawnDog = true;
         }
     }
 }

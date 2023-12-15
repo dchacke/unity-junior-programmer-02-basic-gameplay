9b86b664616edc11ff30872db9da50be08b655d7
Dennis Hackethal <dh@dennishackethal.com>
2023-12-14 20:30:48 -0800

    Vary ball-spawn interval between 3 and 5 seconds
    Using a coroutine as per https://blog.dennishackethal.com/posts/breaking-out-of-frames-in-unity

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,24 +10,31 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float minWait = 3;
+    private float maxWait = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        StartCoroutine("SpawnRandomBall");
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall()
+    private IEnumerator SpawnRandomBall()
     {
-        // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        while (true)
+        {
+            UnityEngine.WaitForSeconds wait = new WaitForSeconds(Random.Range(minWait, maxWait + 1));
 
-        // instantiate ball at random spawn location
-        GameObject ballPrefab = ballPrefabs[Random.Range(0, 3)];
-        Instantiate(ballPrefab, spawnPos, ballPrefab.transform.rotation);
+            // Generate random ball index and random spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+            // instantiate ball at random spawn location
+            GameObject ballPrefab = ballPrefabs[Random.Range(0, 3)];
+            Instantiate(ballPrefab, spawnPos, ballPrefab.transform.rotation);
+
+            yield return wait;
+        }
     }
 
 }
